e868627225d09bae514cb388095fb7facf430cc4
Eri Ferdian <erriferdian92@gmail.com>
2022-11-02 16:23:55 +0700

    sekuel dan percabangan

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 """
 Demo Project Pertama Python
 """
-print("Hello World")
-print("Eri Ferdian")
+
+# Sekuensial
+print('Ibu Berkata, "Budi Tolong Pergi Ke Toko"')
+print('Budi Jawab, "Baik, Beli Apa Bu?"')
+print('Ibu Berkata, "Tolong beli 1 Susu"')
+print('Ibu Berkata, "Jika Ada Telur, Beli 6"')
+print('Budi Berangkat')
+
+# percabangan
+botol_susu = 2
+jumlah_telur = 15
+
+if botol_susu > 0:
+    print('Budi Beli 1 Susu')
+    print('Budi mengecek telur')
+    print(f'Telur Ada {jumlah_telur}')
+    if jumlah_telur > 0:
+        print('Budi Beli 6 Telur')
+    else:
+            print('Budi Tidak Beli Telur')
+else:
+    print(f'Botol Susu Ada {botol_susu}')
+    print('Budi Tidak Jadi Membeli')
+
+print('Budi Pulang Ke Rumah')
