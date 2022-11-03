c7d05e15b4b3e19823e2d66cf2f841deaaa70841
Eri Ferdian <erriferdian92@gmail.com>
2022-11-03 15:45:38 +0700

    perulangan

---
+++ for.py
@@ -0,0 +1,14 @@
+"""
+Program Perulangan
+"""
+
+jumlah_buku = 10
+print('Ibu Berkata, "Baca Semua buku"')
+
+jumlah_buku_baca = 0
+print(f"Buku ke {jumlah_buku_baca} sudah di baca")
+
+for jumlah_buku_baca in range(1, jumlah_buku+1):
+    print(f"Buku ke {jumlah_buku_baca} sudah di baca")
+
+print(jumlah_buku_baca)
---
+++ while.py
@@ -0,0 +1,14 @@
+"""
+Perulangan dengan while
+"""
+
+jumlah_buku = 10
+print('Ibu Berkata, Baca Semua Buku')
+
+jumlah_buku_dibaca = 0
+
+while jumlah_buku_dibaca < jumlah_buku:
+    jumlah_buku_dibaca = jumlah_buku_dibaca +1
+    print(f'Buku ke {jumlah_buku_dibaca} sudah dibaca')
+
+print(f'Jumlah Buku Yang Sudah Di Baca {jumlah_buku_dibaca}')
